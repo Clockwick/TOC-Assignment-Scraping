b05869a71b70c97b976d2716f08c6663f11d5afc
littlelunar <lunar.inthemoonllight@gmail.com>
2022-04-01 18:42:19 +0700

    update: regex html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from calendar import c
+from encodings import utf_8
 from statistics import mode
 from turtle import pd
 from xml.sax import parseString
@@ -22,7 +23,7 @@ async def main():
     page = await browser.newPage()
 
     # Create a URI for our test file
-    page_path = 'https://new.reg.kmitl.ac.th/reg/#/teach_table?mode=by_class&selected_year=2564&selected_semester=2&selected_faculty=01&selected_department=05&selected_curriculum=06&selected_class_year=3&search_all_faculty=false&search_all_department=false&search_all_curriculum=false&search_all_class_year=false'
+    page_path = 'https://new.reg.kmitl.ac.th/reg/#/teach_table?mode=by_class&selected_year=2564&selected_semester=2&selected_faculty=01&selected_department=05&selected_curriculum=06&selected_class_year&search_all_faculty=false&search_all_department=false&search_all_curriculum=false&search_all_class_year=true'
 
     # Open our test file in the opened page
     await page.goto(page_path, {'waitUntil': 'networkidle0'})
@@ -30,51 +31,68 @@ async def main():
 
     # Process extracted content with BeautifulSoup
     soup = BeautifulSoup(page_content, 'html.parser')
-    print(soup.span.string)
+    with open('a.html', mode='w', encoding="utf-8") as f:
+        f.write(soup.prettify())
+    # json_payload = scraping_html(soup.prettify())
 
+    # print(json_payload)
     # Close browser
     await browser.close()
 
 
 def local_scraping_html():
 
+    json_payload = ""
+    a = 'a.html'
+    b = 'schedule-2564.html'
+    with open(b, mode='r', encoding="utf-8") as f:
+
+        soup = BeautifulSoup(f, 'html.parser')
+        # make html in compatible format ( important!!! )
+        json_payload = scraping_html(soup.prettify())
+
+    with open("datahtml.json", mode='w', encoding='utf_8') as jsonfile:
+        jsonfile.write(json_payload)
+
+
+def local_scraping_pdf():
+
+    json_payload = ""
+    with open("mockpdf.txt", mode='r', encoding='utf-8') as pdf:
+        json_payload = scraping_pdf(pdf.read())
+
+    with open("datapdf.json", mode='w', encoding='utf_8') as jsonfile:
+        jsonfile.write(json_payload)
+
+
+def scraping_html(prettified_string):
+
     # {
-    #     "subjects":[
+    #     "subjects": [
     #         {
-    #             "faculty":"",
-    #             "major":"",
-    #             "field":"",
-    #             "field_subjects":[
-    #                 {
+    #             "faculty": "",
+    #             "major": "",
+    #             "field": "",
+    #             "field_subjects": [
+    #                 "type":"",
+    #                 "courses":[
+    #                     "courseId":"",
+    #                     "courseName:"",
+    #                     "credit":"",
+    #                     "section":"",
     #                     "type":"",
-    #                     "courses":[
-    #                         {
-    #                             "courseID":"",
-    #                             "courseName":"",
-    #                             "credit":"",
-    #                             "section":"",
-    #                             "type":"",
-    #                             "schedule":"",
-    #                             "room":"",
-    #                             "building":"",
-    #                             "teacher":"",
-    #                             "midterm":"",
-    #                             "final":"",
-    #                             "restriction":"",
-    #                             "note":""
-    #                         },
-    #                         {
-
-    #                         }
-    #                     ]
-    #                 }
+    #                     "schedule":[],
+    #                     "room":"",
+    #                     "building":"",
+    #                     "teacher":[],
+    #                     "midterm":"",
+    #                     "final":"",
+    #                     "restriction":[],
+    #                     "note":""
+    #                 ]
     #             ]
-    #         },
-    #         {
-
     #         }
-    #     ],
-    #     "semester", ""
+    #     ]
     # }
 
     regex = {
@@ -88,86 +106,104 @@ def local_scraping_html():
 
         "rowData": r'<tr.*?/tr>',
 
-        "data": r'<td><div><span>(?P<courseID>\s*|\d*)</span></div></td><td><a[^>]*>(?P<courseName>\s*|\w+[\s\w]*\w+)</a></td><td>(?P<credit>\s*|\d{1}\(\d{1}-\d{1}-\d{1}\))</td><td[^>]*>(?P<section>\s*|\d+)<span><span[^>]*><span[^>]*>(?P<type>\s*|[\u0E00-\u0E7F]*)</span></span></span></td><td[^>]*><a[^>]*>[<div>]*(?P<schedule>.*?)[</div>]*</a></td><td>(?P<room>.*?)</td><td>(?P<building>.*?)</td><td[^>]*><div>(?P<teacher>.*?)</div></td><td.*?><span.*?/span><span[^>]*>(?P<midterm>.*?)</span>.*?<span.*?/span><span[^>]*>(?P<final>.*?)<.*?/td><td.*?v>(?P<restriction>.*?)</div></td><td.*?v>(?P<note>.*?)<.*?></td>',
+        "data": r'<td><div><.*?>(?P<courseID>\s*|\d*)</.*?></div></td><td><a[^>]*>(?P<courseName>\s*|\w+[\s\w]*\w+)</a></td><td>(?P<credit>\s*|\d{1}\(\d{1,2}-\d{1,2}-\d{1,2}\))</td><td[^>]*>(?P<section>\s*|\d+)<span>(?P<typediv>.*?)</span></td><td[^>]*><a[^>]*>(?P<schedule_all>.*?)</a></td><td>(?P<room>.*?)</td><td>(?P<building>.*?)</td><td[^>]*><div>(?P<teacher>.*?)</div></td><td[^>]*>(?P<exam>.*?)</td><td.*?v>(?P<restriction>.*?)</div></td><td.*?v>(?P<note>.*?)<.*?></td>',
+        
+        "type": r'<.*?>(?P<type>[\u0E00-\u0E7F]*)</span></span>',
+
+        "exam": r'<div><span.*?<span.*?<span[^>]*>(?P<midterm>.*?)</span></div><div><span.*?<span.*?<span[^>]*>(?P<final>.*?)</span></div>',
 
         "teacher": r'<div>(.*?)</div>',
+
+        "schedule": r'<div>(?P<schedule>[+ ]{,2}[\u0E00-\u0E7F]{1,2}. \d{2}:\d{2}-\d{2}:\d{2})</div>',
 
         "restriction": r'<div>(.*?)<div.*?/div></div>'
     }
 
     payload = {}
 
-    with open('schedule-2564.html', mode='r', encoding="utf-8") as f:
+    minify_string = re.sub(
+        r'(?s)<!--.*?-->|\s{3,}|\n', '', str(prettified_string))
 
-        soup = BeautifulSoup(f, 'html.parser')
-        # make html in compatible format ( important!!! )
-        soup = soup.prettify()
+    # get the semester data
+    top = re.search(regex['top'], str(minify_string))
 
-        # minify html for making simpler regex
-        minify_string = re.sub(r'(?s)<!--.*?-->|\s{3,}|\n', '', str(soup))
-        print(minify_string)
-        
-        # get the semester data
-        top = re.search(regex['top'], str(minify_string))
+    payload["semester"] = top.group('semester')
 
-        payload["semester"] = top.group('semester')
+    all_subjects = re.findall(regex['all_subjects'], top.group('alltable'))
 
-        all_subjects = re.findall(regex['all_subjects'], top.group('alltable'))
+    payload["subjects"] = []
 
-        payload["subjects"] = []
+    for field_table in all_subjects:
+        fs = {
+            "faculty": field_table[0],
+            "major": field_table[1],
+            "field": field_table[2],
+            "field_subjects": []
+        }
 
-        for field_table in all_subjects:
-            fs = {
-                "faculty": field_table[0],
-                "major": field_table[1],
-                "field": field_table[2],
-                "field_subjects": []
+        courses_tables = re.findall(regex['courses'], field_table[3])
+
+        for course_table in courses_tables:
+            t = {
+                "type": course_table[0],
+                "courses": []
             }
 
-            courses_tables = re.findall(regex['courses'], field_table[3])
+            courses = re.findall(regex["tbody"], course_table[1])
 
-            for course_table in courses_tables:
-                t = {
-                    "type": course_table[0],
-                    "courses": []
-                }
+            for course in courses:
+                rows = re.findall(regex["rowData"], course)
 
-                courses = re.findall(regex["tbody"], course_table[1])
+                for row in rows:
+                    
+                    data = {}
+                    data_rex = re.search(regex["data"], row)
 
-                for course in courses:
-                    rows = re.findall(regex["rowData"], course)
+                    data['courseId'] = data_rex.group('courseID')
+                    data['courseName'] = data_rex.group('courseName')
+                    data['credit'] = data_rex.group('credit')
+                    data['section'] = data_rex.group('section')
 
-                    for row in rows:
+                    if data_rex.group('typediv') != '':
+                        data['type'] = re.search(
+                            regex['type'], data_rex.group('typediv')).group('type')
+                    else:
+                        data['type'] = ""
 
-                        data_rex = re.search(regex["data"], row)
+                    data['schedule'] = re.findall(
+                        regex['schedule'], data_rex.group('schedule_all'))
 
-                        teacher_list = re.findall(
-                            regex['teacher'], data_rex.group('teacher'))
-                        condition_list = re.findall(
-                            regex['restriction'], data_rex.group('restriction'))
+                    data['room'] = data_rex.group('room')
+                    data['building'] = data_rex.group('building')
 
-                        data = data_rex.groupdict()
+                    data['teacher'] = re.findall(
+                        regex['teacher'], data_rex.group('teacher'))
 
-                        data['teacher'] = teacher_list
-                        data['restriction'] = condition_list
+                    if data_rex.group('exam') != '':
+                        exam = re.search(regex['exam'], data_rex.group('exam'))
+                        data['midterm'] = exam.group('midterm')
+                        data['final'] = exam.group('final')
+                    else:
+                        data['midterm'] = ""
+                        data['final'] = ""
 
-                        t['courses'].append(data)
+                    data['restriction'] = re.findall(
+                        regex['restriction'], data_rex.group('restriction'))
+                    data['note'] = data_rex.group('note')
 
-                fs['field_subjects'].append(t)
+                    t['courses'].append(data)
 
-            payload['subjects'].append(fs)
+            fs['field_subjects'].append(t)
+
+        payload['subjects'].append(fs)
 
     json_payload = json.dumps(
         payload, sort_keys=False, indent=4, ensure_ascii=False)
 
-    # print(json_payload)
-
-    # write into file in order to check the data.
-    with open('datahtml.json', mode='w', encoding="utf-8") as jsonfile:
-        jsonfile.write(json_payload)
+    return json_payload
 
 
-def local_scraping_pdf():
+def scraping_pdf(pdf_string):
 
     # {
     #     "studentId":"",
@@ -217,83 +253,70 @@ def local_scraping_pdf():
 
     payload = {}
 
-    with open("mockpdf.txt", mode='r', encoding='utf-8') as pdf:
-        pdf_string = pdf.read()
+    minify_string = re.sub(r'\n{2,}', '\n', pdf_string)
 
-        minify_string = re.sub(r'\n{2,}', '\n', pdf_string)
-        # print(minify_string)
+    name = re.search(regex['name'], minify_string)
 
-        name = re.search(regex['name'], minify_string)
+    birthdayAndStudentId = re.search(
+        regex['birthdayAndStudentId'], minify_string)
 
-        birthdayAndStudentId = re.search(
-            regex['birthdayAndStudentId'], minify_string)
+    admission = re.search(regex['admission'], minify_string)
 
-        admission = re.search(regex['admission'], minify_string)
+    degree = re.search(regex['degree'], minify_string)
 
-        degree = re.search(regex['degree'], minify_string)
+    major = re.search(regex['major'], minify_string)
 
-        major = re.search(regex['major'], minify_string)
+    cumuGPA = re.search(regex['cumuGPA'], minify_string)
 
-        cumuGPA = re.search(regex['cumuGPA'], minify_string)
+    totalCredit = re.search(regex['totalCredit'], minify_string)
 
-        totalCredit = re.search(regex['totalCredit'], minify_string)
+    semesters = re.findall(regex['semester'], minify_string)
 
-        semesters = re.findall(regex['semester'], minify_string)
+    subjects = []
+    for semester in semesters:
 
-        subjects = []
-        for semester in semesters:
-            # print(semester)
-            # print()
-            semester_data = {
-                "semester": semester[0],
-                "year": semester[1],
-                "GPS": semester[3],
-                "GPA": semester[4],
-                "courses": []
+        semester_data = {
+            "semester": semester[0],
+            "year": semester[1],
+            "GPS": semester[3],
+            "GPA": semester[4],
+            "courses": []
+        }
+
+        courses_insem = re.findall(regex['courseInfo'], semester[2])
+
+        for course in courses_insem:
+            course_data = {
+                "courseId": course[0],
+                "courseName": "",
+                "credit": course[2],
+                "grade": course[3]
             }
+            # print(course)
+            remain = ""
+            if course[4] != "":
+                remain = " " + course[4]
 
-            courses_insem = re.findall(regex['courseInfo'], semester[2])
-            # print(courses_insem)
-            # print()
+            course_data["courseName"] = course[1] + remain
 
-            for course in courses_insem:
-                print(course)
-                print()
-                course_data = {
-                    "courseId": course[0],
-                    "courseName": "",
-                    "credit": course[2],
-                    "grade": course[3]
-                }
-                # print(course)
-                remain = ""
-                if course[4] != "":
-                    remain = " " + course[4]
+            semester_data['courses'].append(course_data)
 
-                course_data["courseName"] = course[1] + remain
+        subjects.append(semester_data)
 
-                semester_data['courses'].append(course_data)
-
-            subjects.append(semester_data)
-
-        payload['studentId'] = birthdayAndStudentId.group('studentId')
-        payload['name'] = name.group('name')
-        payload['birthday'] = birthdayAndStudentId.group('birthday')
-        payload['admission_year'] = admission.group('admission_year')
-        payload['degree'] = degree.group('degree')
-        payload['major'] = major.group('major')
-        payload['total_credit'] = totalCredit.group('total_credit')
-        payload['cumuGPA'] = cumuGPA.group('cumuGPA')
-        payload['subjects'] = subjects
+    payload['studentId'] = birthdayAndStudentId.group('studentId')
+    payload['name'] = name.group('name')
+    payload['birthday'] = birthdayAndStudentId.group('birthday')
+    payload['admission_year'] = admission.group('admission_year')
+    payload['degree'] = degree.group('degree')
+    payload['major'] = major.group('major')
+    payload['total_credit'] = totalCredit.group('total_credit')
+    payload['cumuGPA'] = cumuGPA.group('cumuGPA')
+    payload['subjects'] = subjects
 
     json_payload = json.dumps(
         payload, sort_keys=False, indent=4, ensure_ascii=False)
 
-    # print(json_payload)
-
-    # write into file in order to check the data.
-    with open('datapdf.json', mode='w', encoding="utf-8") as jsonfile:
-        jsonfile.write(json_payload)
+    return json_payload
 
 
 def test_regex(s, regex):
@@ -305,13 +328,13 @@ def test_regex(s, regex):
 
 # asyncio.get_event_loop().run_until_complete(main())
 
-#}}}
+# }}}
 
 # For Local development #{{{
 # ----------------------------------------------------------
 
 # test_regex()
-local_scraping_pdf()
-# local_scraping_html()
+# local_scraping_pdf()
+local_scraping_html()
 
-#}}}
+# }}}
